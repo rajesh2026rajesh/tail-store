d9cc220fc1742039b0d92023ef4adc87a0c3a526
rajesh2026rajesh <rajesh@superintech.com>
2026-09-04 17:48:13 +0530

    updates on 04-09-26

---
+++ home-page/home.js
@@ -0,0 +1,38 @@
+let images = ["../Assets/first-js.svg","../Assets/second-ts.svg","../Assets/third-php.svg","../Assets/forth-tailwind.svg","../Assets/fifth-laravel.svg","../Assets/sixth-react.svg"];
+let index = 0;
+function next() {
+    index++;
+    if(index >=imges.length){
+        index = 0;
+    }
+document.getElementById("slide").src = image[index];
+}
+
+function previous() {
+    index--;
+    if(index <0){
+        index = images.length -1;
+    }
+    document.getElementById("slide").src = image[index];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
