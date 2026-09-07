87f08fe71093db29049285a47d798bed1db4421a
rajesh2026rajesh <rajesh@superintech.com>
2026-09-07 10:33:24 +0530

    update 07-09-26

--- home-page/home.js
+++ home-page/home.js
@@ -1,4 +1,4 @@
-let image = ["../Assets/first-js.svg","../Assets/second-ts.svg","../Assets/third-php.svg","../Assets/forth-tailwind.svg","../Assets/fifth-laravel.svg","../Assets/sixth-react.svg"];
+let image = ["../Assets/first-js.svg","../Assets/second-ts.svg","../Assets/third-php.svg","../Assets/forth-tailwind.svg","../Assets/fifth-laravel.svg","../Assets/sixth-react.svg","../Assets/right-arrow-brand.png"];
 let index = 0;
 function next() {
     index++;
@@ -10,7 +10,7 @@ document.getElementById("brand-image").src = image[index];
 
 function previous() {
     index--;
-    if(index <0){
+    if(index < 0){
         index = image.length -1;
     }
     document.getElementById("brand-image").src = image[index];
